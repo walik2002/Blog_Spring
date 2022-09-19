7766ab8780bb7fa8c157205893235983819fb736
Valetin Belousow <bel2002v@yandex.ru>
2022-09-19 23:09:54 +0300

    Page navigation

--- src/main/java/com/example/blog/controllers/MainController.java
+++ src/main/java/com/example/blog/controllers/MainController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     @GetMapping("/")
-    public String greeting(Model model) {
+    public String home(Model model) {
         model.addAttribute("title", "Главная страница");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Страница про нас");
+        return "about";
     }
 
 }
