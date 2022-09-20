52f23d188473ce6b1c3defe25096e666e5e287ce
Valetin Belousow <bel2002v@yandex.ru>
2022-09-20 11:19:39 +0300

    Database dependency

--- src/main/java/com/example/blog/controllers/BlogController.java
+++ src/main/java/com/example/blog/controllers/BlogController.java
@@ -1,13 +1,22 @@
 package com.example.blog.controllers;
 
+import com.example.blog.models.Post;
+import com.example.blog.repo.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BlogController {
+
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/blog")
     public String blogMain(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 }
